51945f6420e0ad40ac689e85db39b9bd66bb8eb2
ElijahJunaid <121450694+ElijahJunaid@users.noreply.github.com>
2024-12-19 16:58:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 function showFederal() {
-    document.getElementById('section-title').innerText = 'Federal';
+    document.getElementById('section-title').innerText = 'Federal Criminal Code';
     document.getElementById('federal-content').style.display = 'flex';
     document.getElementById('municipal-content').style.display = 'none';
 }
 
 function showMunicipal() {
-    document.getElementById('section-title').innerText = 'Municipal';
+    document.getElementById('section-title').innerText = 'Municipal Criminal Code';
     document.getElementById('federal-content').style.display = 'none';
     document.getElementById('municipal-content').style.display = 'flex';
 }
 
 function showFRCP() {
-    document.getElementById('section-title').innerText = 'FRCP';
+    document.getElementById('section-title').innerText = 'Federal Rules of Civil Procedure';
     document.getElementById('frcp-content').style.display = 'flex';
     document.getElementById('frcmp-content').style.display = 'none';
 }
 
 function showFRCMP() {
-    document.getElementById('section-title').innerText = 'FRCMP';
-    document.getElementById('frcmp-content').style.display = 'flex';
+    document.getElementById('section-title').innerText = 'Federal Rules of Criminal Procedure';
     document.getElementById('frcp-content').style.display = 'none';
+    document.getElementById('frcmp-content').style.display = 'flex';
+}
+
+function showConstitution() {
+    document.getElementById('section-title').innerText = 'Constitution';
+    document.getElementById('constitution-content').style.display = 'flex';
+    document.getElementById('amendment-content').style.display = 'none';
+}
+
+function showAmendments() {
+    document.getElementById('section-title').innerText = 'Constitutional Amendments';
+    document.getElementById('constitution-content').style.display = 'none';
+    document.getElementById('amendment-content').style.display = 'flex';
 }
