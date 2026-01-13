552cdbbf72fa2ff0f5e24ec40ad468bde390f9cb
elijahjunaid <realelijahjunaid@gmail.com>
2026-01-13 16:33:18 -0500

    Remove CSP report-only header and extend static asset cache to 1 year with immutable directive

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,12 @@ export default defineNuxtConfig({
         devServer: {
             watch: [],
         },
+        serverAssets: [
+            {
+                baseName: "prisma",
+                dir: "prisma",
+            },
+        ],
     },
 
         app: {
